3a539d4c06e41c1116d9907421fd5cfd6a6d8e74
Farhan Hasan <hasanfarhan33@gmail.com>
2021-08-03 23:21:06 +0300

    Learnt about draggable and resizable

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-$("#draggable").draggable();
+$("#verticalDraggable").draggable({ axis: "y" });
+$("#horizontalDraggable").draggable({ axis: "x" });
+$("#stuckText").draggable({ containment: "parent" });
+$("#resizableBox").resizable({ aspectRatio: true, autoHide: true });
