96310ed322b878c0a0bc6a7a4c53236cbe6001f3
Farhan Hasan <hasanfarhan33@gmail.com>
2021-08-04 18:59:10 +0300

    Learn about accordians, sortables, draggablesandmo

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-$("#verticalDraggable").draggable({ axis: "y" });
-$("#horizontalDraggable").draggable({ axis: "x" });
-$("#stuckText").draggable({ containment: "parent" });
-$("#resizableBox").resizable({ aspectRatio: true, autoHide: true });
+// $("#verticalDraggable").draggable({ axis: "y" });
+// $("#horizontalDraggable").draggable({ axis: "x" });
+// $("#stuckText").draggable({ containment: "parent" });
+// $("#resizableBox").resizable({ aspectRatio: true, autoHide: true });
+
+// $("#draggable").draggable();
+// $("#droppable").droppable({
+//   drop: function (event, ui) {
+//     alert("Dropped");
+//   },
+// });
+
+// $("#accordion").accordion();
+
+$("#sortable").sortable({
+  cursor: "move",
+});
+$("#sortable").disableSelection();
